b38b3c3aca4af011b20cf651a52f82c45846534e
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2025-07-18 15:42:17 +0530

    post card updated

--- Blogsy/src/components/PostCard.jsx
+++ Blogsy/src/components/PostCard.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import SpotlightCard from './Animation/SpotLightCard';
 import { useEffect } from 'react';
 import { formatDistanceToNow } from 'date-fns';
+import parse from "html-react-parser";
 
 
 function PostCard(post) {
@@ -31,9 +32,10 @@ function PostCard(post) {
                   />
               </div>
               <h2
-              className='text-sm sm:text-lg p-4 font-bold'
+              className='text-sm sm:text-lg px-4 py-2 font-bold text-black/60'
               >{post.title}</h2>
-              <div className='flex p-4 items-center font-medium text-xs sm:text-sm' >
+              <p className='px-4 py-2 text-xs text-black/50 ' >{parse(post?.content?.substring(0,250).trim())}</p>
+              <div className='flex px-4 py-2 items-center font-medium text-xs sm:text-sm' >
                 <div className='bg-blue-300 rounded-full w-10 h-10 mr-4 flex justify-center items-center text-black/50 font-semibold text-2xl font-mono bg-gradient-to-r from-[#e0aef8]  to-[#8cacf6]' >
                   {post?.username?.[0]?.toUpperCase()}
                 </div>
